a18fd0323571ee66dd6fb1f5418032e75d433a02
gherkins05 <finlaysperkins@gmail.com>
2025-02-19 10:56:47 +0000

    Completed worksheet

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -11,6 +11,44 @@ export function subtract(a, b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
+}
+
+export function compare(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+
+  return true;
+}
+
+export function largest(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+export function zeroest(arr) {
+  let closest = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (Math.abs(arr[i]) < Math.abs(closest)) {
+      closest = arr[i];
+    }
+  }
+
+  return closest;
 }
